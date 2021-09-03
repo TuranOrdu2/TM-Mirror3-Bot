466019ce2e5867da2dca547e85dc2601329f2443
TuranOrdu <90020994+TuranOrdu@users.noreply.github.com>
2021-09-03 09:43:20 +0430

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("👥 Leech Grupu", "https://t.me/LeechGrupu")
+    buttons.buildbutton("🎦 Kanal ", "https://t.me/TuranMovies")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -56,7 +56,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
         sendMarkup(
-            'Oops! not a Authorized user.\nPlease deploy your own <b>slam-mirrorbot</b>.',
+            'Vay! Botu Kullanma Yetkiniz Yok.\n Lütfen Grupa Katılın  <b>Leech Grupu ⚡</b>.',
             context.bot,
             update,
             reply_markup,
